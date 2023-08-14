0fb536d24e237f0965b24f18fc9c90d3443e0a1a
Kayure <52473139+Kayure@users.noreply.github.com>
2023-08-14 17:27:22 -0300

    Update routes.ts

--- node-api/src/routes/routes.ts
+++ node-api/src/routes/routes.ts
@@ -13,16 +13,6 @@ router.delete('/deleteUser/:id', ApiController.deleteUser);
 router.patch('/updateUser/:id', ApiController.updateUser);
 
 
-router.get('/fibonacci', (req, res) => {
-    const number = fb.iterate(10000);
-    res.send(number);
-});
-
-// router.get('/fibonacciThreaded', async (req, res) => {
-//     runFibonacci({ iterations: 10000 }).then(result => log.info(result));
-//     res.send('processing');
-//   });
-
 
 
 
