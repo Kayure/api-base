6332655cea6b41abb5c8320dc5521183a37e7d0b
Kayure <52473139+Kayure@users.noreply.github.com>
2023-08-16 09:21:09 -0300

    Update routes.ts

--- node-api/src/routes/routes.ts
+++ node-api/src/routes/routes.ts
@@ -6,11 +6,9 @@ import * as ApiController from '../controllers/apiController';
 const router = Router();
 
 router.post('/registerUser', ApiController.register);
-router.post('/login', ApiController.login);
-router.get('/listEmail', ApiController.listEmail);
+
 router.get('/listAll', ApiController.listAll);
-router.delete('/deleteUser/:id', ApiController.deleteUser);
-router.patch('/updateUser/:id', ApiController.updateUser);
+
 
 
 
